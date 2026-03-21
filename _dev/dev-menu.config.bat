e4e45e3447c17bbc8b22047c1df27c22ecacaa98
LiuYangArt <liuyangart@gmail.com>
2026-03-21 16:18:12 +0800

    Update dev menu config for LG TV 6.0 simulator

--- _dev/dev-menu.config.bat
+++ _dev/dev-menu.config.bat
@@ -1,4 +1,4 @@
 @echo off
-set "DEVICE=tv"
-set "SIMULATOR_VERSION=25"
-set "SIMULATOR_PATH=E:\Portable Apps\Media\webOS_TV_25_Simulator_1.4.4"
+set "DEVICE=lgtv"
+set "SIMULATOR_VERSION=6.0"
+set "SIMULATOR_PATH=E:\Portable Apps\Media\webOS_TV_6.0_Simulator_1.4.1"
